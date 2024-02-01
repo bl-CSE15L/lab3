55420b67f14d061918f6a4effd870ded16d3b694
bl-CSE15L <156377155+bl-CSE15L@users.noreply.github.com>
2024-02-01 14:53:22 -0800

    Bug Fixing

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,22 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
-
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test 
+	public void testReverseInPlaceLen3() {
+    int[] input = {0, 1, 2};
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[]{2, 1, 0}, input);
+	}
+
+  @Test 
+	public void testReversedLen3() {
+    int[] input = {0, 1, 2};
+    assertArrayEquals(new int[]{2, 1, 0}, ArrayExamples.reversed(input));
+	}
 }
