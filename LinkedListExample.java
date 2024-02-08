c01a256e1b149619e4b1ce10f3370a96bae90e59
bl-CSE15L <156377155+bl-CSE15L@users.noreply.github.com>
2024-02-08 14:03:14 -0800

    Update LinkedListExample.java

--- LinkedListExample.java
+++ LinkedListExample.java
@@ -20,7 +20,7 @@ class LinkedList {
      */
     public void prepend(int value) {
         // Just add at the beginning
-        this.root = new Node(value, null);
+        this.root = new Node(value, this.root);
     }
     /**
      * Adds the value to the _end_ of the list
